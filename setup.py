c1443eef4a071e0e3e62b60fd077977ec5059701
ofnewman <oscarn@lumico.io>
2022-08-23 10:55:41 +0100

    Version update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='Flask-Inputs',
-    version='0.2.0',
+    version='0.3.1',
     description='Flask request data validation',
     long_description=long_description,
     url='http://pythonhosted.org/Flask-Inputs/',
